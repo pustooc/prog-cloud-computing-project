def85f9e32578e90372101d64b1a5a816f8e57c7
infoomics <sbx_94@hotmail.com>
2024-12-12 21:35:55 +0000

    Specify port when running

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.use('/api/user', authRoute);
 mongoose.connect(process.env.DB_CONNECTOR).then(() => { console.log('Your mongoDB connector is on...')})
 
 app.listen(3000, ()=>{
-    console.log('Server is running')
+    console.log('Server is running on port 3000')
 })
